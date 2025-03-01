38cb4d50b770967400ff484d4d1b19e6c536e3ef
IndhuMuruganantham <157617725+IndhuMuruganantham@users.noreply.github.com>
2025-03-01 13:33:21 +0530

    Create number of employee who met with target.c

---
+++ number of employee who met with target.c
@@ -0,0 +1,9 @@
+int numberOfEmployeesWhoMetTarget(int* hours, int n, int target) {
+    int c=0;
+    for(int i=0;i<n;i++){
+        if(hours[i]>=target){
+            c=c+1;
+        }
+    }
+    return c;
+}
